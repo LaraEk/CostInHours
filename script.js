b1d38b363efbb8acc2dfb0ea6aee30cab50c7b42
Ekscursionist <larakek@gmail.com>
2018-11-12 18:05:14 -0500

    adding css to boxes; adding basic javascript;creating citations

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("script.js is connected and working");
+
+$(document).ready(function(){
+
+$("#nationalwagebutton").on("click", function() {
+    alert("I clicked it");
+    console.log("I clicked it");
+    
+    
+
+})
+
+
+
+
+
+}); //to document.readyfunction
