a1b626cdad9ee29307ff8408e863eec27ec46dac
Ekscursionist <larakek@gmail.com>
2018-11-13 07:25:21 -0500

    divs

--- script.js
+++ script.js
@@ -2,11 +2,29 @@ console.log("script.js is connected and working");
 
 $(document).ready(function(){
 
+//NATL MIN WAGE
+    const natlminwage = 7.25;
+
+// EMPTY CONSTS
+    const costoffood = 0;
+    const costofgas = 0;
+    const costofmetro = 0;
+
+//FOOD
+    const familyfouroneweek = 200;
+
+//GAS
+    const fillasmalltank = 33;
+
+//METRO
+    const metroparkinig = 4.85;
+
 $("#nationalwagebutton").on("click", function() {
     alert("I clicked it");
     console.log("I clicked it");
     
-    
+    $("#costoffooddiv").
+
 
 })
 
